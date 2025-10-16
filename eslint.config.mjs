4500649fd91e2b4122cbc1ebb12652a2e40ba157
Lukas Lieb <lukas.lieb@axonivy.com>
2025-10-16 09:07:39 +0200

    Use default hook rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,8 +14,6 @@ export default tseslint.config(
       }
     }
   },
-  // Latest react-hook rules
-  ...config.reactHooksLatest,
   // Custom rules for core package
   {
     name: 'packages/core',
